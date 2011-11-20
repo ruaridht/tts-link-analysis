8d59348d26ab13f3637c31b428f0b9bc0911c3d4
Ruaridh Thomson <echelous@me.com>
2011-11-20 16:05:47 +0000

    pagerank working

--- engine.py
+++ engine.py
@@ -7,23 +7,33 @@ s0786036
 Practical 4 - Link Analysis
 """
 
-import re
-import math
-import time
 import string
 
 LAMBDA = 0.8
-ITERATIONS = 10 #should be 10
+ITERATIONS = 10
+
+class Node(object):
+  def __init__(self, name, dest_nodes=None, source_nodes=None):
+    self.name         = name
+    self.dest_nodes   = dest_nodes
+    self.source_nodes = source_nodes
+    
+    if not self.dest_nodes:
+      self.dest_nodes = []
+      
+    if not self.source_nodes:
+      self.source_nodes = []
+    
+  def getNumNodes(self):
+    return len(self.dest_nodes)
+  
+  def getNumSource(self):
+    return len(self.source_nodes)
 
 class PageRank(object):
-  def __init__(self):
-    self.graph       = [] # This may just be a waste of memory...
-    self.senders     = []
-    self.receivers   = []
-    self.uniqueNodes = []
-    self.uLen        = 0  # this is N and is used many times
-    self.pr_t        = []
-    self.pr_t1       = []
+  def __init__(self, nodes_dict, nodes_num = None):
+    self.nodes_dict = nodes_dict
+    self.nodes_num  = len(self.nodes_dict)
     
   def writeOut(self):
     f = open('pr.txt','w')
@@ -31,92 +41,111 @@ class PageRank(object):
       f.write(rank + "\n")
     f.close()
     print ">>> Top 10 PageRanks written to: pr.txt"
-    
-  def _loadGraph(self):
-    print ">>> Loading graph.txt..."
-    f = open('graph.txt','r')
-    lines = f.readlines()
-    for line in lines:
-      # 0=IDs 1=emailer 2=emailee
-      lineSplit = string.split(line)
-      
-      # If the sender is emailing himself we should ignore
-      if (lineSplit[1] == lineSplit[2]):
-        continue
-      
-      self.graph.append(lineSplit) # populate graph.txt to array
-      self.senders.append(lineSplit[1]) # populate senders
-      self.receivers.append(lineSplit[2]) # populate receivers
-      
-      # Do we count emailees in the node graph?
-      self.uniqueNodes.append(lineSplit[1])
-      self.uniqueNodes.append(lineSplit[2])
-    f.close()
-    
-    # Remove duplicates from the unique list
-    self.uniqueNodes = list(set(self.uniqueNodes))
-    self.uLen = len(self.uniqueNodes)
   
-  def _stats(self):
-    print "Graph size: ", len(self.graph)
-    print "Unique nodes: ", len(self.uniqueNodes)
-    print "Num senders: ", len(self.senders)
-    print "Num receivers: ", len(self.senders)
-    
-  # For x, return the indices in senders for all y for y->x
-  def _all_indices(self, item):
-    indices = []
-    idx = -1
-    while 1:
-      try:
-        idx = self.receivers.index(item,idx+1)
-        indices.append(idx)
-      except ValueError:
-        break
-    return indices
+  def normalise(self,vector):
+    sum = reduce(lambda s,el: s+el,vector.values())
+    # incase sum is 0
+    if not sum:
+      sum = 1
+    return dict([(u,float(c)/sum) for (u,c) in vector.items( )])
   
-  def _sum_for_node(self, ni):
-    s = 0.0
-    inds = self._all_indices(ni)
+  def rank(self):
+    print ">>> Beginning PageRank..."
+    # init pr_t and pr_t1 as dicts
+    pr_t = {}
+    # populate initial pr
+    for n_name in self.nodes_dict: pr_t[n_name] = 1.0 / self.nodes_num
+
+    # (1-lambda)/N
+    pre_sum = (1.0 - LAMBDA)/self.nodes_num
     
-    # y to follow slide notation
-    # say y sends multiple emails to x, we will do this loop unneccesarily more
-    for ind in inds:
-      y = self.senders[ind]
-      y_out = self.senders.count(y)
-      pr_y = self.pr_t[self.uniqueNodes.index(y)]
-      s += float(pr_y)/float(y_out)
+    for i in xrange(ITERATIONS):
+      print ">>> Calculating iteration ", i, "..."
+      pr_t1 = {}
       
-    return s
-  
-  def _pr(self):
-    one_m_lambda_n = (1.0 - LAMBDA)/self.uLen
-    for node_index in range(self.uLen):
-      sum_node = self._sum_for_node(node_index)
-      self.pr_t1[node_index] = one_m_lambda_n + (LAMBDA*sum_node)
-    self.pr_t = list(self.pr_t1)
+      for node_x_name in self.nodes_dict:
+        node_x = self.nodes_dict[node_x_name]
+        pr_x = pre_sum
+        
+        sum_y = 0.0
+        for node_y_name in node_x.source_nodes:
+          node_y = self.nodes_dict[node_y_name]
+          # If y doesn't point to x then move on to next y
+          if (node_x_name in node_y.dest_nodes):
+            pr_y = pr_t.get(node_y_name)
+            out_y = node_y.getNumNodes()
+          
+            sum_y += pr_y/(out_y*1.0) # *1.0 to get float
+          else:
+            print "Node x not in node y dests"
+        
+        # if no nodes point to x then sum_y will be 0 and pr_x will be (1-lambda)/N
+        pr_x += (LAMBDA*sum_y)
+        pr_t1[node_x_name] = pr_x
       
-  def _pr_iters(self):
-    for i in range(ITERATIONS):
-      print ">>> Calculating PR for iteration ", i, " ..."
-      self._pr()
+      #pr_t = dict(pr_t1)
+      pr_t = self.normalise(pr_t1)
+      print pr_t.get('jeff.dasovich@enron.com')
+    
+    return pr_t
+    
+def graphAsNodes(edges):
+  print ">>> Loading graph.txt..."
+  nodes = {}
   
-  def getRanks(self):
-    self._loadGraph()
+  for ed in edges:
+    n_from = ed[0]
+    n_to = ed[1]
     
-    self.pr_t = [1.0/self.uLen]*self.uLen
-    self.pr_t1 = [1.0/self.uLen]*self.uLen #just to initialise it
-    print "uLen: ", self.uLen
-    self._pr()
+    # populate edges pointing from nodes
+    if not nodes.has_key(n_from):
+      nodes[n_from] = Node(n_from,[n_to])
+    else:
+      nodes[n_from].dest_nodes.append(n_to)
     
-    self._stats()
+    if not nodes.has_key(n_to):
+      nodes[n_to] = Node(n_to)
     
+    # populate edges pointing to nodes
+    if not nodes.has_key(n_to):
+      nodes[n_to] = Node(n_to,[],[n_from])
+    else:
+      nodes[n_to].source_nodes.append(n_from)
+      
+    if not nodes.has_key(n_from):
+      nodes[n_from] = Node(n_from)
+  
+  return nodes
+
+def getGraphEdges():
+  print ">>> Loading graph.txt..."
+  graph = []
+  
+  f = open('graph.txt','r')
+  lines = f.readlines()
+  
+  for line in lines:
+    # 0=IDs 1=emailer 2=emailee
+    lineSplit = string.split(line)
+    
+    # If the sender is emailing himself we should ignore
+    if (lineSplit[1] == lineSplit[2]):
+      continue
+    
+    graph.append( (lineSplit[1],lineSplit[2]) )
+  f.close()
+  
+  return graph
+
 
 def main():
   print ">>> Beginning Link Analysis"
   
-  pr = PageRank()
-  pr.getRanks()
+  edges = getGraphEdges()
+  nodes = graphAsNodes(edges)
+  
+  page = PageRank(nodes)
+  page.rank()
   
   print ">>> Goodbye."
 
